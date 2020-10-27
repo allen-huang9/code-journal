7fa652443c346b7c87451c6bb8698afd572d3701
Allen Huang <ligerx066@gmail.com>
2020-10-26 23:01:38 -0700

    listen for submit events on form, assign all form values to data model and reset form inputs. #11 - 14

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ var $pfpPreview = document.querySelector('.pfp');
 $avatarUrl.addEventListener('input', function (e) {
   $pfpPreview.setAttribute('src', e.target.value);
 });
+
+var $form = document.querySelector('form');
+
+$form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  data.profile.avatarUrl = $form.elements.avatarUrl.value;
+  data.profile.username = $form.elements.username.value;
+  data.profile.fullName = $form.elements.fullname.value;
+  data.profile.location = $form.elements.location.value;
+  data.profile.bio = $form.elements.bio.value;
+  console.log('username: ', data.profile.username);
+  console.log('full name: ', data.profile.fullName);
+  console.log('location: ', data.profile.location);
+  console.log('bio: ', data.profile.bio);
+
+  $form.elements.avatarUrl.value = '';
+  $form.elements.username.value = '';
+  $form.elements.fullname.value = '';
+  $form.elements.location.value = '';
+  $form.elements.bio.value = '';
+  $pfpPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
+});
