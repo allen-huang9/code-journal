6c309b319a073b65c372bb12e53be65bd4d910b0
Allen Huang <ligerx066@gmail.com>
2020-10-26 23:59:50 -0700

    user data persist in local storage bewtween refresh and removed unnecessary codes. #15-17

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ $avatarUrl.addEventListener('input', function (e) {
 });
 
 var $form = document.querySelector('form');
+var previousEntries = localStorage.getItem('code-journal-local-storage');
+
+if (previousEntries) {
+  data.entries = JSON.parse(previousEntries);
+}
 
 $form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -15,10 +20,8 @@ $form.addEventListener('submit', function (e) {
   data.profile.fullName = $form.elements.fullname.value;
   data.profile.location = $form.elements.location.value;
   data.profile.bio = $form.elements.bio.value;
-  console.log('username: ', data.profile.username);
-  console.log('full name: ', data.profile.fullName);
-  console.log('location: ', data.profile.location);
-  console.log('bio: ', data.profile.bio);
+
+  data.entries.push(data.profile);
 
   $form.elements.avatarUrl.value = '';
   $form.elements.username.value = '';
@@ -26,4 +29,9 @@ $form.addEventListener('submit', function (e) {
   $form.elements.location.value = '';
   $form.elements.bio.value = '';
   $pfpPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
+});
+
+window.addEventListener('beforeunload', function (e) {
+  var stringData = JSON.stringify(data.entries);
+  localStorage.setItem('code-journal-local-storage', stringData);
 });
